e5c62ab4f3d093aa2e5573e378529040a7af4749
kakie <782922086@qq.com>
2022-11-02 22:19:29 +0800

    scrapy

---
+++ scrapy_url/src/main.rs
@@ -0,0 +1,44 @@
+use std::fs;
+
+fn main() {
+    // unwrap();
+
+    match wrap() {
+        Ok(result) => println!("{:?}", result),
+        Err(e) => println!("Error: {:?}", e.to_string()),
+    }
+}
+
+fn unwrap() {
+    let url = "https://www.rust-lang.org/";
+    let output = "rust.md";
+
+    println!("Fetching url: {}", url);
+    let body = reqwest::blocking::get(url).unwrap().text().unwrap();
+
+    println!("Converting html to markdown...");
+    let md = html2md::parse_html(&body);
+
+    fs::write(output, md.as_bytes()).unwrap();
+    println!("Converted markdown has been saved in {}.", output);
+}
+
+fn wrap() -> Result<(), Box<dyn std::error::Error>> {
+    let url = "https://www.rust-lang.org/";
+    let output = "rust.md";
+
+    println!("Fetching url: {}", url);
+    let body = reqwest::blocking::get(url)?.text()?;
+
+    println!("Converting html to markdown...");
+    let md = html2md::parse_html(&body);
+
+    fs::write(output, md.as_bytes())?;
+    println!("Converted markdown has been saved in {}.", output);
+
+    // Ok for anything right
+    // Ok(())
+
+    // Err for something wrong
+    Err(Box::from("helo"))
+}
