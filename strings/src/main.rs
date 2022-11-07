d05b811247a3c3e2f5eae9d4b968f33db3dc4b1b
kakie <782922086@qq.com>
2022-11-07 13:30:20 +0800

    colorize

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,7 +1,14 @@
+use colored::Colorize;
+
 fn main() {
     let a = "Hello world!";
     let b = a;
     // let c = a.as_ref();
     let d: &str = a.as_ref();
     println!("{:?}", d);
+
+    let e = "haha".to_string().blue();
+    let f = "hehe".red();
+    println!("{}", e);
+    println!("{}", f);
 }
