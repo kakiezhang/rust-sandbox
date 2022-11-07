2cac682e57b95d80803f7ae3961358cac91133d4
kakie <782922086@qq.com>
2022-11-07 13:18:48 +0800

    httpie

--- httpie/src/main.rs
+++ httpie/src/main.rs
@@ -1,7 +1,19 @@
+use anyhow::{anyhow, Result};
 use clap::Parser;
+use colored::Colorize;
+use mime::Mime;
+use reqwest::{header, Client, Response, Url};
+use std::collections::HashMap;
+use std::str::FromStr;
+use syntect::{
+    easy::HighlightLines,
+    highlighting::{Style, ThemeSet},
+    parsing::SyntaxSet,
+    util::{as_24_bit_terminal_escaped, LinesWithEndings},
+};
 
 #[derive(Parser, Debug)]
-#[command(version = "1.0", author = "kakie")]
+#[clap(version = "1.0", author = "kakie")]
 struct Opts {
     #[clap(subcommand)]
     subcmd: SubCommand,
@@ -9,22 +21,138 @@ struct Opts {
 
 #[derive(Parser, Debug)]
 enum SubCommand {
+    /// http get method
     Get(Get),
+    /// http post method
     Post(Post),
 }
 
 #[derive(Parser, Debug)]
 struct Get {
+    /// http get url
+    #[clap(parse(try_from_str = parse_url))]
     url: String,
+}
+
+fn parse_url(s: &str) -> Result<String> {
+    let _url: Url = s.parse()?;
+    Ok(s.into())
+}
+
+fn parse_kv_pair(s: &str) -> Result<KvPair> {
+    s.parse()
 }
 
 #[derive(Parser, Debug)]
 struct Post {
+    /// http post url
+    #[clap(parse(try_from_str = parse_url))]
     url: String,
-    body: Vec<String>,
+    /// http post body
+    #[clap(parse(try_from_str = parse_kv_pair))]
+    body: Vec<KvPair>,
 }
 
-fn main() {
+#[derive(Debug)]
+struct KvPair {
+    k: String,
+    v: String,
+}
+
+impl FromStr for KvPair {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split = s.split("=");
+        let err = || anyhow!(format!("failed to parse {}", s));
+        Ok(Self {
+            k: (split.next().ok_or_else(err)?).to_string(),
+            v: (split.next().ok_or_else(err)?).to_string(),
+        })
+    }
+}
+
+// 打印服务器版本号 + 状态码
+fn print_status(resp: &Response) {
+    let status = format!("{:?} {}", resp.version(), resp.status()).blue();
+    println!("{}\n", status);
+}
+
+// 打印服务器返回的 HTTP header
+fn print_headers(resp: &Response) {
+    for (name, value) in resp.headers() {
+        println!("{}: {:?}", name.to_string().green(), value);
+    }
+
+    println!();
+}
+
+/// 将服务器返回的 content-type 解析成 Mime 类型
+fn get_content_type(resp: &Response) -> Option<Mime> {
+    resp.headers()
+        .get(header::CONTENT_TYPE)
+        .map(|v| v.to_str().unwrap().parse().unwrap())
+}
+
+/// 打印服务器返回的 HTTP body
+fn print_body(m: Option<Mime>, body: &str) {
+    match m {
+        // 对于 "application/json" 我们 pretty print
+        Some(v) if v == mime::APPLICATION_JSON => print_syntect(body, "json"),
+        Some(v) if v == mime::TEXT_HTML => print_syntect(body, "html"),
+
+        // 其它 mime type，我们就直接输出
+        _ => println!("{}", body),
+    }
+}
+
+fn print_syntect(s: &str, ext: &str) {
+    // Load these once at the start of your program
+    let ps = SyntaxSet::load_defaults_newlines();
+    let ts = ThemeSet::load_defaults();
+    let syntax = ps.find_syntax_by_extension(ext).unwrap();
+    let mut h = HighlightLines::new(syntax, &ts.themes["base16-ocean.dark"]);
+    for line in LinesWithEndings::from(s) {
+        let ranges: Vec<(Style, &str)> = h.highlight(line, &ps);
+        let escaped = as_24_bit_terminal_escaped(&ranges[..], true);
+        print!("{}", escaped);
+    }
+}
+
+async fn get(cli: Client, args: &Get) -> Result<()> {
+    let resp = cli.get(&args.url).send().await?;
+    Ok(print_resp(resp).await?)
+}
+
+async fn post(cli: Client, args: &Post) -> Result<()> {
+    let mut body = HashMap::new();
+    for pair in args.body.iter() {
+        body.insert(&pair.k, &pair.v);
+    }
+    let resp = cli.post(&args.url).json(&body).send().await?;
+    Ok(print_resp(resp).await?)
+}
+
+async fn print_resp(resp: Response) -> Result<()> {
+    print_status(&resp);
+    print_headers(&resp);
+    let mime = get_content_type(&resp);
+    let body = resp.text().await?;
+    print_body(mime, &body);
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
     let opts: Opts = Opts::parse();
-    println!("{:?}", opts);
+    // println!("sub: {:?}", opts.subcmd);
+    let client = reqwest::Client::builder().build()?;
+    let result = match opts.subcmd {
+        SubCommand::Get(ref args) => get(client, args).await?,
+        SubCommand::Post(ref args) => {
+            // println!("h1 {:?}", args);
+            post(client, args).await?;
+        }
+    };
+    Ok(result)
 }
