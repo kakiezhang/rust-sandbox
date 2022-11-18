27622bae516366821d38c13ab5b86ec0f8dd6085
kakie <782922086@qq.com>
2022-11-18 16:09:17 +0800

    rc ref

--- rc/src/main.rs
+++ rc/src/main.rs
@@ -1,10 +1,21 @@
 use std::rc::Rc;
 
 fn main() {
-    let x = Rc::new(10);
-    let y = match Rc::try_unwrap(x) {
-        Ok(v) => v,
-        Err(e) => panic!("ERR: {:?}", e),
-    };
-    println!("{:?}", y);
+    {
+        let x = Rc::new(10);
+        let _y = x.clone();
+        // let _y = Rc::clone(&x); // same as the previous line
+        let y = *x;
+        println!("y0: {:?}", y);
+        println!("y1: {:?}", y);
+        assert_eq!(y, 10);
+    }
+
+    {
+        let x = Rc::new(4);
+        let _y = Rc::clone(&x);
+        assert_eq!(*x, 4);
+        assert_eq!(*Rc::try_unwrap(x).unwrap_err(), 4);
+        // assert_eq!(*x, 4);
+    }
 }
