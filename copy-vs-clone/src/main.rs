44d0a37bd3510ca3425ea5f824c2f8f7e71744c4
kakie <782922086@qq.com>
2023-02-01 22:34:51 +0800

    copy nested within not copy

--- copy-vs-clone/src/main.rs
+++ copy-vs-clone/src/main.rs
@@ -87,34 +87,85 @@ fn test_goods_1() {
     assert_eq!(a.goods.v, b.goods.v);
 }
 
-// #[test]
-// fn test_goods_2() {
-//     let g0: &str = "cup";
-//     let g1: &str = "fish";
-//     let g2: &str = "meat";
-//     let g3: &str = "eggs";
+// -----------------
 
-//     let goods = vec![g0, g1, g2, g3];
+#[derive(Debug, Clone)]
+struct School {
+    id: u32,
+    name: String,
+    grade: Grade,
+}
 
-//     let mut a = Supermarket {
-//         goods: Goods { v: &goods },
-//     };
+#[derive(Debug, Clone, Copy)]
+struct Grade {
+    id: u32,
+    class: u8,
+}
 
-//     let b = Supermarket {
-//         goods: Goods { v: &goods },
-//     };
+fn test_school() {
+    let s = School {
+        id: 1,
+        name: "fudan".to_string(),
+        grade: Grade { id: 3, class: 8 },
+    };
 
-//     *a.goods.v.get_mut(0).unwrap() = "water";
+    let mut s1 = s.clone();
+    s1.name = "jiaoda".to_string();
+    s1.grade = Grade { id: 4, class: 5 };
 
-//     // let g4: &str = "water";
-//     // if let Some(elem) = a.goods.v.get_mut(0) {
-//     //     *elem = g4;
-//     // }
+    println!("s: {:?}, {:p}, {:?}", s, s.name.as_str(), s.name.as_ptr());
+    println!(
+        "s1: {:?}, {:p}, {:?}",
+        s1,
+        s1.name.as_str(),
+        s1.name.as_ptr()
+    );
+}
 
-//     println!("a: {:?}", a);
-//     println!("b: {:?}", b);
+fn test_school_1() {
+    let s = School {
+        id: 1,
+        name: "fudan".to_string(),
+        grade: Grade { id: 3, class: 8 },
+    };
+    println!("s: {:?}, {:p}, {:?}", s, s.name.as_str(), s.name.as_ptr());
 
-//     assert_eq!(a.goods.v, b.goods.v);
-// }
+    let mut s1 = s;
+    s1.name = "jiaoda".to_string();
+    s1.grade = Grade { id: 4, class: 5 };
 
-fn main() {}
+    println!(
+        "s1: {:?}, {:p}, {:?}",
+        s1,
+        s1.name.as_str(),
+        s1.name.as_ptr()
+    );
+}
+
+fn test_grade() {
+    let g = Grade { id: 1, class: 2 };
+    let g1 = g;
+    println!("g: {:?}, {:p}", g, &g);
+    println!("g1: {:?}, {:p}", g1, &g1);
+}
+
+#[derive(Debug)]
+struct Fruit {
+    name: String, // String doesn't imple Copy trait
+}
+
+fn test_fruit() {
+    let f = Fruit {
+        name: "apple".to_string(),
+    };
+    println!("f: {:?}, {:p}", f, &f);
+    let f1 = f;
+    println!("f1: {:?}, {:p}", f1, &f1);
+}
+
+fn main() {
+    // test_school();
+    // test_school_1();
+    // test_grade();
+    test_fruit();
+}
