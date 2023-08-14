aa5105f55d0317ee830ad15a3e1dcac6d3b04095
kakie <782922086@qq.com>
2023-08-14 22:09:26 +0800

    config struct with env

--- env-conf/src/main.rs
+++ env-conf/src/main.rs
@@ -1,10 +1,28 @@
-// APP_CONF_PATH=1 cargo run
+use serde::{Deserialize, Serialize};
+
+// APP_CONF_PATH=config.toml cargo run
 fn main() {
     let mut settings = config::Config::default();
     settings
         .merge(config::Environment::new().prefix("APP"))
         .unwrap();
-    println!("{:?}", settings);
+    // println!("{:?}", settings);
     let conf_path = settings.get_str("conf_path").unwrap();
-    println!("{:?}", conf_path);
+    // println!("{:?}", conf_path);
+    settings.merge(config::File::with_name(&conf_path)).unwrap();
+    println!("settings: {:?}", settings);
+
+    let botconf: BotConfig = settings.clone().try_into().unwrap();
+    println!("botconf: {:?}", botconf);
+}
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub struct BotConfig {
+    pub conf_path: String,
+    pub sys: Sys,
+}
+
+#[derive(Debug, Serialize, Deserialize, Default, Clone)]
+pub struct Sys {
+    name: String,
 }
